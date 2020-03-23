ddfd968fba4c39e36dbe79f758bc083917e83302
Atanas Kozhuharov <atanas.kozhuharov@sumup.com>
2020-03-23 18:05:31 +0200

    person update

--- person.py
+++ person.py
@@ -1,16 +1,21 @@
 class Person:
     def __init__(self, name, age):
-        self.age = age
         self.name = name
+        self.age = age
 
     def __add__(self, other_obj):
-        return self.age + other_obj.age
+        return str(self.age) + other_obj.name
 
-    def __str__(self):
-        return "My name is {} ".format(self.name)
+    def __sub__(self, other_obj):
+        return self.age - other_obj.age
+
+    def __mul__(self, other_obj):
+        return self.age * other_obj.age
+
+    def __truediv__(self, other_obj):
+        return self.age / other_obj.age
 
 
-
-p1 = Person("Goshko", 15)
-p2 = Person("Ivan", 14)
-print(p2)
+p = Person("Alex", 15)
+p1 = Person("Pesho", 10)
+print(p /p1)
