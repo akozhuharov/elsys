77c89bc667c2d94549ba0eaaaef48b2a62b45cf4
Atanas Kozhuharov <atanas.kozhuharov@sumup.com>
2020-03-23 18:44:28 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='elsys',
+      version='1.0.0',
+      description='My elsys package',
+      url='http://github.com/atkozhuharov/elsys',
+      author='Atanas K',
+      author_email='atkozhuharov@gmail.com',
+      license='MIT',
+      packages=['elsys'],
+      zip_safe=False)
